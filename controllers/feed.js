68e71dad1fc8d4f13f6b20aa37f23240ff69f073
GideonBature <infoaboutgideon@gmail.com>
2024-04-29 03:10:37 +0100

    set up routes, controllers and app for a get response

---
+++ controllers/feed.js
@@ -0,0 +1,10 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        title: "First Post",
+        content: "This is the first post",
+      },
+    ],
+  });
+};
