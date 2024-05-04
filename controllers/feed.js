0c0a9dad42dd20d9b2f5a8800a97f1f0f52090ac
GideonBature <infoaboutgideon@gmail.com>
2024-05-04 11:08:04 +0100

    add to code

--- controllers/feed.js
+++ controllers/feed.js
@@ -8,3 +8,17 @@ exports.getPosts = (req, res, next) => {
     ],
   });
 };
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = {
+    id: new Date().toISOString(),
+    title: title,
+    content: content,
+  };
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: post,
+  });
+};
